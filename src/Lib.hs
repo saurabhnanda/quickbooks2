57f59bd314a9c8ef34c1d657b6db4a3ab2554139
Saurabh Nanda <saurabh@vacationlabs.com>
2018-11-27 13:16:54 +0530

    trying to parse all XSD files

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,16 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Fadno.Xml.ParseXsd as Xsd
+import qualified Fadno.Xml.EmitTypes as Emit
+
+parseXsd :: IO (Emit.EmitState)
+parseXsd = do
+  restSchema <- Xsd.parseFile "xsd/IntuitRestServiceDef.xsd"
+  financeSchema <- Xsd.parseFile "xsd/Finance.xsd"
+  baseTypesSchema <- Xsd.parseFile "xsd/IntuitBaseTypes.xsd"
+  nameTypesSchema <- Xsd.parseFile "xsd/IntuitNamesTypes.xsd"
+  reportSchema <- Xsd.parseFile "xsd/Report.xsd"
+  salesTaxSchema <- Xsd.parseFile "xsd/SalesTax.xsd"
+  xsdSchema <- Xsd.loadXsdSchema "xsd/XMLSchema.xsd"
+  let allSchemas = restSchema <> xsdSchema <> baseTypesSchema <> nameTypesSchema <> financeSchema <> reportSchema <> salesTaxSchema
+  snd <$> (Emit.runEmit (Emit.Env allSchemas) mempty (Emit.emitSchema allSchemas))
